6e025b07beabfee5d0790e7224172894f1a6fe88
satamkundu <satamkundu67@gmail.com>
2025-04-27 21:16:08 +0530

    login and registration api creation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request){
+        $user = User::where('email', $request->validated()['email'])->first();
+
+        if (! $user || ! Hash::check($request->validated()['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+    public function register(RegisterRequest $request){
+         try {
+            $user = User::create([
+                'name' => $request->validated()['name'],
+                'email' => $request->validated()['email'],
+                'password' => Hash::make($request->validated()['password']),
+            ]);
+
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json([
+                'user' => $user,
+                'token' => $token,
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Failed to register.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request){
+        try {
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->json([
+                'message' => 'Logged out successfully'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Failed to logout.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
